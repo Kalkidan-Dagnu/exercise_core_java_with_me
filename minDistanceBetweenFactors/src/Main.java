7d320a470ccc63c56f077da9ac091b0b21ac8411
Kalkidan D <kalkidan.dagnu@sarasanalytics.com>
2024-09-18 14:15:22 +0300

    add min distance between factors

---
+++ minDistanceBetweenFactors/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(minDistance(13013));
+    }
+
+    private static int minDistance(int num) {
+        int minDis = Integer.MAX_VALUE;
+        int[] factors = getFactors(num);
+        for(int i = 1; i < factors.length; i++) {
+            minDis = Math.min(factors[i] - factors[i - 1], minDis);
+        }
+        return minDis;
+    }
+
+    private static int[] getFactors(int num) {
+        int[] factors = new int[num];
+        int count = 0;
+        for(int i = 1; i < Math.sqrt(num); i++) {
+            if(num % i == 0) {
+                factors[count++] = i;
+                if(i != (num / i)) {
+                    factors[count++] = num/i;
+                }
+            }
+        }
+        int[] result = Arrays.copyOf(factors,count);
+        Arrays.sort(result);
+        return result;
+    }
+}
